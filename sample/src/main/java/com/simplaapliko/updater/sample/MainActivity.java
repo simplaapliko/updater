be3ca93b7f9b16b7b66ab9f325c4d56ee1e31b84
Oleg Kan <oleg.kan@hotmail.com>
2018-12-01 20:49:32 -0500

    migrate to androidX

--- sample/src/main/java/com/simplaapliko/updater/sample/MainActivity.java
+++ sample/src/main/java/com/simplaapliko/updater/sample/MainActivity.java
@@ -17,8 +17,8 @@
 package com.simplaapliko.updater.sample;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
--- sample/src/main/java/com/simplaapliko/updater/sample/MainFragment.java
+++ sample/src/main/java/com/simplaapliko/updater/sample/MainFragment.java
@@ -18,8 +18,8 @@ package com.simplaapliko.updater.sample;
 
 import android.content.Context;
 import android.content.DialogInterface;
-import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
